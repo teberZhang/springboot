87369eb6b49435d64c0d18525ec8081c05550c08
zhangshengyong <bnyt_zhangsy@163.com>
2021-11-19 09:48:15 +0800

    定时任务

---
+++ src/main/java/com/example/springboot/schedule/TestTask.java
@@ -0,0 +1,28 @@
+package com.example.springboot.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@EnableScheduling
+@Component
+@Slf4j
+public class TestTask {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(initialDelay = 3 * 1000, fixedDelay = 10 * 1000)
+    public void testTodo() {
+
+        log.info("testTodo begin-----：" + dateFormat.format(new Date()));
+        try {
+            log.info("testTask todo");
+        } catch (Throwable e) {
+            log.error(e.getMessage());
+        }
+        log.info("testTodo end-------：" + dateFormat.format(new Date()));
+    }
+}
